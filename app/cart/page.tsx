eada86fd9730f0e80e68d89d5512dacb23914810
huzaifa <huzaifazaki92@gmail.com>
2024-12-09 01:06:44 +0500

    first commit

---
+++ app/cart/page.tsx
@@ -0,0 +1,103 @@
+"use client"
+import React from 'react'
+import { integralCF } from '@/style/fonts'
+import { useState } from 'react'
+
+import shirt1 from '@/public/Images/pic1.png'
+import shirt2 from '@/public/Images/pic2.png'
+import shirt3 from '@/public/Images/pic3.png'
+import Image from 'next/image'
+
+
+const cartData = [
+  {
+    name:"T-SHIRT WITH TAPE DETAILS",
+    size:"M",
+    color:"BLACK",
+    price:"$145",
+    img: shirt1
+  },
+  {
+    name:"CHECKERED SHIRT",
+    size:"M",
+    color:"BLACK",
+    price:"$180",
+    img: shirt2
+  },
+  {
+    name:"SKINNY FIT JEANS",
+    size:"M",
+    color:"BLACK",
+    price:"$240",
+    img: shirt3
+  }
+]
+
+const page : React.FC = () => {
+  const [quantity, setQuantity] = useState(1)
+  return (
+    <section className='min-h-screen bg-white'>
+      <div className='pt-4 mx-auto w-[80vw]'>
+        <h1 className={`${integralCF.className} text-4xl`}>YOUR CART</h1>
+        <div className='flex flex-col justify-around items-center gap-4 border border-gray rounded-3xl mt-10'>
+          {cartData.map((item) => (
+            <>
+            
+            
+            <div className='flex justify-center items-center  w-full'>
+              <Image src={item.img} alt={item.name} width={150} className='rounded-3xl px-4'/>
+              <div className='flex flex-col py-4 border-b border-gray p-8 w-full'>
+              <h3 className='text-[rgba(0,0,0,0.8)] font-bold text-xl '>{item.name}</h3>
+              <div className='flex flex-col justify-between'>
+                  <p className='text-[rgba(0,0,0,0.8)]'>Size: {item.size}</p>
+                  <p className='text-[rgba(0,0,0,0.8)]'>Color: {item.color}</p>
+                  <span className='flex flex-row justify-between items-center '>
+                  <p className='text-[rgba(0,0,0,0.8)] font-bold text-xl'>{item.price}</p>
+                  <div className='flex flex-row gap-4 justify-center items-center w-[50%] md:w-[30%] bg-[#f0f0f0] rounded-3xl'>
+                  <button className='text-4xl ' onClick={() => (quantity > 1)? setQuantity(quantity - 1) : setQuantity (1)}>-</button>
+                  <p className='font-bold text-xl'>{quantity}</p>
+                  <button className='text-2xl' onClick={() => setQuantity(quantity + 1)}>+</button>
+                </div>
+                  </span>        
+              </div>
+              </div>
+            </div>
+            </>
+          ))}
+        </div>
+        <div className='flex flex-col gap-4 border border-gray rounded-3xl mt-10 p-8'>
+          <div>
+            <h3 className='text-black font-bold text-xl text-left'>Order Summary</h3>
+            <div>
+              <span className='flex flex-row justify-between'>
+                <p className='text-[rgba(0,0,0,0.8)] text-left'>Subtotal</p>
+                <p className='font-bold'>$565</p>
+              </span>
+              <span className='flex flex-row justify-between'>
+                <p className='text-[rgba(0,0,0,0.8)] text-left '>Discount (20%) </p>
+                <p className='text-red-600 font-bold'>-$133</p>
+              </span>
+              <span className='flex flex-row justify-between'>
+                <p className='text-[rgba(0,0,0,0.8)] text-left'>Delivery Fees</p>
+                <p className='font-bold'>$15</p>
+              </span>
+            </div>
+            <hr className="mx-auto w-full h-[4px] border-black/20 my-2" /> 
+            <span className='flex flex-row justify-between'>
+                <p className='text-[rgba(0,0,0,0.8)] text-left'>Total</p>
+                <p className='font-bold text-xl'>$467</p>
+              </span>
+          </div>
+          <div className='flex justify-between gap-2'>
+            <input type="text" placeholder="Promo Code" className="focus:outline-none w-[70%] p-2 border border-gray rounded-3xl" />
+            <button className="text-center bg-black text-white py-3 px-4 rounded-full w-[30%]">Apply</button>
+          </div>
+          <button className='text-center bg-black text-white py-3 px-4 rounded-full '>Checkout</button>
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default page
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,8 +4,71 @@ import Image from 'next/image'
 import pic1 from '@/public/Images/pic9.png'
 import pic2 from '@/public/Images/pic10.png'
 import pic3 from '@/public/Images/pic11.png'
+import shirt1 from '@/public/Images/pic1.png'
+import shirt2 from '@/public/Images/pic2.png'
+import shirt3 from '@/public/Images/pic3.png'
+import shirt4 from '@/public/Images/pic4.png'
+import star from '@/public/Icons/small-star.svg'
 import { integralCF } from '@/style/fonts'
 import { useState } from 'react'
+
+
+const reviews = [
+  {
+    name:"Sarah M",
+    review:`""I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.”"`,
+    stars:5
+  },
+  {
+    name:"Alex K.",
+    review:`"Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.”`,
+    stars:4
+  },
+  {
+    name:"James L.",
+    review:`"As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.”`,
+    stars:5,
+  },
+]
+
+type Products = {
+  name: string
+  image: string | any
+  price: number
+  discountprice: number
+  rating: number
+}
+
+const NewProducts: Products[] = [
+  {
+    name: "T-SHIRT WITH TAPE DETAILS",
+    image: shirt1,
+    price: 200,
+    discountprice: 100,
+    rating: 4,
+  },
+  {
+    name: "SKINNY FIT JEANS",
+    image: shirt2,
+    price: 200,
+    discountprice: 100,
+    rating: 3,
+  },
+  {
+    name: "CHECKERED SHIRT",
+    image: shirt3,
+    price: 200,
+    discountprice: 100,
+    rating: 5,
+  },
+  {
+    name: "SLEEVE STRIPED T-SHIRT",
+    image: shirt4,
+    price: 200,
+    discountprice: 100,
+    rating: 4,
+  },
+]
 
 const page = () => {
   const [quantity, setQuantity] = useState(1)
@@ -59,13 +122,39 @@ const page = () => {
         
         
     </div>
-    <div className='w-[80vw] mx-auto mt-10 select-none'>
-          <span className='flex justify-around items-center gap-4'>
+    <div className='w-screen mx-auto mt-10 select-none'>
+          <span className='w-[80%] mx-auto flex justify-around items-center gap-4'>
             <h3 className='border-b-2 text-[rgba(0,0,0,0.6)] border-black/20 hover:text-black hover:border-black cursor-pointer'>Product Details</h3>
             <h3 className='border-b-2 border-black cursor-pointer'>Rating & Reviews</h3>
             <h3 className='border-b-2 text-[rgba(0,0,0,0.6)] border-black/20 hover:text-black hover:border-black cursor-pointer'>FAQs</h3>
           </span>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-4 place-items-center w-screen p-4 bg-white'>
+          {reviews.map((review) => (
+            <div className='flex flex-col justify-center gap-4 px-6 py-8 bg-white border border-gray rounded-xl h-[250px] max-w-[400px]'>
+              <span className='flex items-center'>{[...Array(review.stars)].map((_, index) => <Image key={index} src={star} alt='star'/>)}</span>
+              <h3 className='text-[rgba(0,0,0,0.8)] font-bold text-xl '>{review.name}</h3>
+              <p className='text-[rgba(0,0,0,0.8)] overflow-clip'>{review.review}</p>
+            </div>  
+          ))}
+      </div>
+    </div>
+    <div className='w-screen mx-auto flex flex-col justify-center items-center'>
+      <h2 className={`${integralCF.className} text-4xl font-bold mt-10 mx-auto`}>YOU MIGHT ALSO LIKE</h2>
+      <div className='flex flex-row justify-center items-start w-[75%] rounded-full p-6 gap-4'>
+          {NewProducts.map((product) => (
+            <div className='flex flex-col justify-center items-start'>
+            <Image src={product.image} alt="product"  className=' bg-[#f0f0f0] cursor-pointer hover:shadow-2xl rounded-xl'/>
+            <h2 className='font-bold text-lg pt-2'>{product.name}</h2>
+            <span className='flex items-center'>{[...Array(product.rating)].map((_, index) => <Image key={index} src={star} alt='star'/>)}</span>
+
+            <span className='flex justify-start gap-5 w-full'>
+              <p className='font-bold text-xl pt-2 '>${product.discountprice}</p>
+              <p className='font-bold text-xl pt-2 line-through text-[rgba(0,0,0,0.4)]'>${product.price}</p>
+            </span>
+            </div>
+          ))}
         </div>
+    </div>
     </section>
     
     
